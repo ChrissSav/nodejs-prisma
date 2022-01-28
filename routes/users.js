52ff35d72142be3c82dc14edd65c8ca1ced2589a
ChrissSav <dai17023@uom.edu.gr>
2022-01-28 20:45:56 +0200

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { PrismaClient } = require('@prisma/client');
+const { user } = new PrismaClient();
+
+router.get('/', async (req, res) => {
+  let users = await user.findMany({
+    select: {
+      id: true,
+      username: true,
+    },
+  });
+
+  res.send(users);
+});
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  const userExists = await user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      username: true,
+    },
+  });
+
+  if (userExists) {
+    return res.status(400).json({
+      msg: 'user already exists',
+    });
+  }
+
+  let newUser = await user.create({
+    data: {
+      username,
+      password,
+    },
+    select: {
+      username: true,
+      id: true,
+    },
+  });
+
+  res.send(newUser);
+});
+
+module.exports = router;
